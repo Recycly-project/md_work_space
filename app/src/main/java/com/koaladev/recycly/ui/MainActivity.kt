576bd73b5ba9dcb9f77b162669e2aa16f8289c2c
romiyusnandar <yusromi04@gmail.com>
2024-11-13 08:36:09 +0700

    Move bottomNavBar to navigation implementation

--- app/src/main/java/com/koaladev/recycly/ui/MainActivity.kt
+++ app/src/main/java/com/koaladev/recycly/ui/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.koaladev.recycly.ui
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.koaladev.recycly.R
 import com.koaladev.recycly.databinding.ActivityMainBinding
@@ -15,43 +21,38 @@ class MainActivity : AppCompatActivity() {
 
     // initialize view binding
     private lateinit var binding: ActivityMainBinding
-
-    // initialize bottom navigation bar and fragment manager
-    private lateinit var bottomNavBar: BottomNavigationView
+    // inittialize navController
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // set up bottom navigation bar and fragment manager
-        bottomNavBar = binding.bottomNavigation
-        bottomNavBar.setOnItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.bottom_home -> {
-                    setupBottomNavBar(HomeFragment())
-                    true
-                }
-                R.id.bottom_history -> {
-                    setupBottomNavBar(HistoryFragment())
-                    true
-                }
-                R.id.bottom_point -> {
-                    setupBottomNavBar(PointFragment())
-                    true
-                }
-                R.id.bottom_user_profile -> {
-                    setupBottomNavBar(UserProfileFragment())
-                    true
-                }
-                else -> false
-            }
-        }
-        setupBottomNavBar(HomeFragment())
+        setupEdgeToEdge()
+        setupNavigation()
     }
 
-    private fun setupBottomNavBar(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.main, fragment).commit()
+    private fun setupEdgeToEdge() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            binding.root.setPadding(insets.left, insets.top, insets.right, 0)
+            binding.bottomNavbar.setPadding(0, 0, 0, insets.bottom)
+
+            WindowInsetsCompat.CONSUMED
+        }
     }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavbar.setupWithNavController(navController)
+
+        // soon handle reselection to prevent unnecessary navigation
+        binding.bottomNavbar.setOnItemReselectedListener { /* Do nothing */ }
+    }
+
 }
 
