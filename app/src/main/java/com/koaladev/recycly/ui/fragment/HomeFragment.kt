e146f61fb771a90d5abff50cc6cbee07381f1af7
romiyusnandar <yusromi04@gmail.com>
2024-11-26 22:09:26 +0700

    Revert "Add RecyclyViewModel for apply MVVM"
    This reverts commit d3a2c7215426882dde52c4b212ea40a9d4b54dc2.

--- app/src/main/java/com/koaladev/recycly/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/koaladev/recycly/ui/fragment/HomeFragment.kt
@@ -1,48 +1,28 @@
 package com.koaladev.recycly.ui.fragment
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.koaladev.recycly.databinding.FragmentHomeBinding
-import com.koaladev.recycly.ui.viewmodel.RecyclyViewModel
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
-
-    private val viewModel: RecyclyViewModel by viewModels()
+    private lateinit var _binding: FragmentHomeBinding
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.viewModel = viewModel
         return binding.root
     }
 
-    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.points.observe(viewLifecycleOwner) { points ->
-            binding.tvPointsValue.text = points.toString()
-        }
-
-//        binding.btnScanTrash.setOnClickListener() {
-//            viewModel.addPoints(5)
-//        }
-
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        binding.tvPointsValue.text = "150"
     }
 }
