b7ecb57e2a7b193165f887da4ecd2a3b8376a8b0
romiyusnandar <yusromi04@gmail.com>
2024-12-10 08:18:33 +0700

    Move to MainActivity when splash done

--- app/src/main/java/com/koaladev/recycly/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/koaladev/recycly/ui/activity/SplashActivity.kt
@@ -1,43 +1,43 @@
 package com.koaladev.recycly.ui.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.koaladev.recycly.R
 import androidx.constraintlayout.motion.widget.MotionLayout
-import com.airbnb.lottie.LottieAnimationView
+import com.koaladev.recycly.databinding.ActivitySplashBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
-    private lateinit var motionLayout: MotionLayout
-    private lateinit var lottieAnimationView: LottieAnimationView
+    private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        enableEdgeToEdge()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        motionLayout = findViewById(R.id.motionLayout)
-        lottieAnimationView = findViewById(R.id.lottieAnimationView)
-
-        lottieAnimationView.addAnimatorUpdateListener { valueAnimator ->
+        binding.lottieAnimationView.addAnimatorUpdateListener { valueAnimator ->
             if (valueAnimator.animatedFraction == 1f) {
-                motionLayout.transitionToEnd()
+                binding.motionLayout.transitionToEnd()
             }
         }
 
-        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
             override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {}
 
             override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {}
 
             override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-                 startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                 startActivity(Intent(this@SplashActivity, MainActivity::class.java))
                  finish()
             }
 
             override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) {}
         })
 
-        lottieAnimationView.playAnimation()
+        binding.lottieAnimationView.playAnimation()
     }
 }
